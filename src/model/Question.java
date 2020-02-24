cfdaac71e1518f0f7078158251a9e92cf7ea463b
Ming Qiu <mqiu@ucdavis.edu>
2020-02-24 09:45:01 -0800

    first commit

---
+++ src/model/Question.java
@@ -0,0 +1,84 @@
+package model;
+import common.Utils;
+
+import java.util.*;
+import java.io.File;
+
+public class Question {
+    private String questionContent;
+    private String topic;
+    private String questionType;
+    private List<String> choices;
+    private List<String> answers;
+    private int points;
+    private String pictureName;
+    private String audioName;
+    private boolean isAnswered = false;
+
+    public Question(String questionContent, String topic, String questionType, List<String> choices, List<String> answers, int points, String pictureName, String audioName) {
+        this.questionContent = questionContent;
+        this.topic = topic;
+        this.questionType = questionType;
+        this.choices = choices;
+        this.answers = answers;
+        this.points = points;
+        this.pictureName = pictureName;
+        this.audioName = audioName;
+    }
+
+    public boolean isAnsweredRight(List<String> userAnswer) {
+        if (userAnswer == null) {
+            return false;
+        }
+        if (userAnswer.size() != this.answers.size()) {
+            return false;
+        }
+        for (String answer : this.answers) {
+            if (!userAnswer.contains(answer)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isAnswered(){
+        return this.isAnswered;
+    }
+
+    public void questionAnswered(){
+        this.isAnswered = true;
+    }
+
+    public String getQuestionContent() {
+        return this.questionContent;
+    }
+
+    public String getTopic() {
+        return this.topic;
+    }
+
+    public String getQuestionType() {
+        return this.questionType;
+    }
+
+    public List<String> getChoices() {
+        return this.choices;
+    }
+
+    public List<String> getAnswers() {
+        return this.answers;
+    }
+
+    public int getPoints() {
+        return this.points;
+    }
+
+    public String getPictureFilePath(){
+        return new File(Utils.PICATURE_DIR, String.format("%s.jpeg", pictureName)).getPath();
+    }
+
+    public String getAudioFilePath(){
+        return new File(Utils.PICATURE_DIR, String.format("%s.jpeg", audioName)).getPath();
+    }
+
+}
---
+++ src/model/QuestionBank.java
@@ -0,0 +1,141 @@
+package model;
+
+import common.Utils;
+
+import java.util.*;
+
+public class QuestionBank {
+    private static Map<String, List<Question>> topicQuestionMap = new HashMap<String, List<Question>>();
+
+    private String currentTopic = null;
+    private Question currentQuestion = null;
+
+    /**
+     * The constructor method, loading all the questions into QuestionBank
+     */
+    public QuestionBank() {
+        loadQuestions(Utils.QUESTIONS_FILE);
+    }
+
+    public static void main(String[] args) {
+        loadQuestions(Utils.QUESTIONS_FILE);
+    }
+
+//    /**
+//     * Choose an unanswered question in a specified topic and notify observers
+//     * @param topic topic of the question
+//     */
+//    public void publish(String topic){
+//        currentTopic = topic;
+//        chooseQuestion(topic);
+//
+//        setChanged();
+//        notifyObservers(currentQuestion);
+//    }
+
+    private static void loadQuestions(String inputQuestionsPath){
+        String questionsContent = Utils.readFile(inputQuestionsPath);
+        String[] questions = questionsContent.split("<%>");
+        for (int i = 0; i < questions.length; i++) {
+            String question = questions[i];
+            String questionContent = "";
+            String topic = "";
+            String questionType = "";
+            List<String> choices = new ArrayList<>();
+            List<String> answers = new ArrayList<>();
+            int points = -1;
+            String pictureName = "";
+            String audioName = "";
+            if (question.length() > 0) {
+                String[] content = question.split("<#>");
+                questionContent = content[0];
+                topic = content[1];
+                questionType = content[2];
+                String[] choicesString = content[3].split("<c>");
+                for (int c = 0; c < choicesString.length; c++) {
+                    choices.add(choicesString[c]);
+                }
+                String[] answersString = content[4].split("<c>");
+                for (int c = 0; c < answersString.length; c++) {
+                    answers.add(answersString[c]);
+                }
+                points = Integer.valueOf(content[5]);
+                pictureName = content[6];
+                audioName = content[7];
+                System.out.println(questionContent);
+                System.out.println(topic);
+                System.out.println(questionType);
+                Question questionObj = new Question(questionContent, topic, questionType, choices, answers, points, pictureName, audioName);
+                addQuestion(questionObj);
+            }
+        }
+        System.out.println(" =====");
+    }
+
+    private static void addQuestion(Question question){
+        String topic = question.getTopic();
+        if(!topicQuestionMap.containsKey(topic)){
+            List<Question> questions = new ArrayList<>();
+            topicQuestionMap.put(topic, questions);
+        }
+
+        List<Question> questions = topicQuestionMap.get(topic);
+        questions.add(question);
+    }
+
+    private void chooseQuestion(String topic){
+        boolean allAnswered = true;
+        List<Question> topicQuestions = topicQuestionMap.get(topic);
+        if(topicQuestions == null)
+            return;
+        // Check if all questions have been answered to avoid infinite loop in the random selection
+        for(Question question : topicQuestions){
+            if(!question.isAnswered())
+                allAnswered = false;
+        }
+
+        if(allAnswered){
+            currentQuestion = null;
+            return;
+        }
+
+        int totalQuestions = topicQuestions.size();
+        Random rand = new Random();
+
+        while(true){
+            int questionIndex = rand.nextInt(totalQuestions);
+            Question question = topicQuestions.get(questionIndex);
+            if(!question.isAnswered()){
+                currentQuestion = question;
+                break;
+            }
+        }
+    }
+
+    /**
+     * Get the topic of question that the user is answering
+     * @return the topic of question which is being answered
+     */
+    public String getCurrentTopic() {
+        return currentTopic;
+    }
+
+    /**
+     * Get the current question that the user is answering
+     * @return the question which is being answered
+     */
+    public Question getCurrentQuestion(){
+        return currentQuestion;
+    }
+
+    /**
+     * Get all the topics in question bank
+     * @return a list of question topics
+     */
+    public List<String> getAllTopics(){
+        List<String> allTopics = new ArrayList<>();
+        for(String key : topicQuestionMap.keySet())
+            allTopics.add(key);
+        return allTopics;
+    }
+}
